6e3b73a9f771d589969358df58bdeb72f4a7fff7
asupe950 <148280007+asupe950@users.noreply.github.com>
2023-10-17 13:15:33 -0700

    Update Tower.java

--- src/Tower.java
+++ src/Tower.java
@@ -3,14 +3,15 @@ import processing.core.PApplet;
 public class Tower {
     private float x, y;
     private float width, height;
-    private int hp;
-    public Tower (float x, float y, float width, float height) {
+
+    public Tower(float x, float y, float width, float height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-    public void draw (PApplet window) {
+
+    public void draw(PApplet window) {
         window.fill(200, 185, 200);
         window.rect(x, y, width, height);
     }
